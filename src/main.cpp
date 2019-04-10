82498b7766c051c384d98061d42cd696b8d73027
Alexej Harm <alexej.h@xiphos.de>
2019-04-10 16:34:46 +0200

    removed threads dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <config.hpp>
 #include <core/test.hpp>
 #include <fmt/format.h>
+#include <filesystem>
 #include <cstdio>
 
 int main(int argc, char* argv[])
@@ -12,4 +13,5 @@ int main(int argc, char* argv[])
   std::puts("Version Text: " VERSION_TEXT);
   std::puts(fmt::format("Version Info: {}.{}.{}", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH).data());
   std::puts(fmt::format("Library Test: {}", core::test()).data());
+  std::puts(std::filesystem::current_path().u8string().data());
 }

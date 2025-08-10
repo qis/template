df984dee69be2a2515faddf0334d585adf5f334d
Alexej Harm <alexej.h@xiphos.de>
2025-08-10 11:21:35 +0300

    restored vcpkg-configuration.json and added remote cache

--- readme.md
+++ readme.md
@@ -1,9 +1,10 @@
 # Template
 C++20 project template that uses:
 
-* [qis/vcpkg](https://github.com/qis/vcpkg) - a mirrored vcpkg repository
-* [qis/ports](https://github.com/qis/ports) - a custom ports overlay
+* [`qis/vcpkg`](https://github.com/qis/vcpkg) - a mirrored vcpkg repository
+* [`qis/ports`](https://github.com/qis/ports) - a custom ports overlay
 
+## CMake
 ```cmd
 :: Build project and run tests.
 cmake --workflow x64-debug
@@ -16,4 +17,72 @@ cmake --workflow x64-release
 
 :: Manually run benchmarks.
 build\x64-release\bin\benchmarks.exe
+```
+
+## Cache
+Uses a local cache in `.cache/vcpkg/binaries` if the environment variable `VCPKG_BINARY_SOURCES`
+in [`CMakePresets.json`](CMakePresets.json) is not set.
+
+The remove cache was tested on an nginx server with the following config:
+
+```nginx
+worker_processes 1;
+
+events {
+  worker_connections 1024;
+}
+
+http {
+  include mime.types;
+  default_type application/octet-stream;
+
+  sendfile on;
+  #tcp_nopush on;
+  keepalive_timeout 65;
+  #gzip  on;
+
+  map $time_iso8601 $timestamp {
+    ~^([0-9-]+)T([0-9:]+) "$1 $2";
+  }
+
+  map $remote_addr $address {
+    ~^(..............) "$1 ";
+    ~^(.............) "$1  ";
+    ~^(............) "$1   ";
+    ~^(...........) "$1    ";
+    ~^(..........) "$1     ";
+    ~^(.........) "$1      ";
+    ~^(........) "$1       ";
+    ~^(.......) "$1        ";
+    default $remote_addr;
+  }
+
+  log_format access '[$timestamp] $address $status "$request" $body_bytes_sent';
+
+  server {
+    listen 80;
+    server_name localhost;
+
+    access_log logs/access.log access;
+
+    location / {
+      root html;
+      index index.html index.htm;
+    }
+
+    #error_page 404 /404.html;
+    error_page 500 502 503 504 /50x.html;
+    location = /50x.html {
+      root html;
+    }
+
+    location /vcpkg/cache {
+      alias vcpkg/cache/;
+      dav_methods PUT;
+      dav_access user:rw group:rw all:rw;
+      client_max_body_size 0;
+      autoindex on;
+    }
+  }
+}
 ```
--- res/vcpkg.cmake
+++ res/vcpkg.cmake
@@ -1,4 +1,5 @@
 include_guard(GLOBAL)
+# Visual Studio
 set(VSWHERE_ROOT "$ENV{ProgramFiles\(x86\)}/Microsoft Visual Studio/Installer")
 cmake_path(CONVERT "${VSWHERE_ROOT}" TO_CMAKE_PATH_LIST VSWHERE_ROOT NORMALIZE)
 
@@ -7,6 +8,7 @@ list(APPEND VSWHERE -nologo -nocolor -utf8 -latest -property resolvedInstallatio
 execute_process(COMMAND ${VSWHERE} ENCODING UTF-8 OUTPUT_VARIABLE VS_ROOT OUTPUT_STRIP_TRAILING_WHITESPACE)
 cmake_path(CONVERT "${VS_ROOT}" TO_CMAKE_PATH_LIST VS_ROOT NORMALIZE)
 
+# Vcpkg
 find_program(VCPKG NAMES vcpkg REQUIRED PATHS "${VS_ROOT}/VC/vcpkg")
 get_filename_component(VCPKG_ROOT "${VCPKG}" DIRECTORY)
 
@@ -15,20 +17,28 @@ set(VCPKG_INSTALL_OPTIONS "${VCPKG_INSTALL_OPTIONS};--clean-buildtrees-after-bui
 set(VCPKG_INSTALL_OPTIONS "${VCPKG_INSTALL_OPTIONS};--clean-packages-after-build")
 set(VCPKG_INSTALL_OPTIONS "${VCPKG_INSTALL_OPTIONS};--no-print-usage")
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchain.cmake")
+
+if(NOT DEFINED ENV{VCPKG_BINARY_SOURCES})
+  set(ENV{VCPKG_BINARY_SOURCES} "clear;files,${CMAKE_SOURCE_DIR}/.cache/vcpkg/binaries,readwrite")
+endif()
+
 include("${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
 
+# Libraries
 file(GLOB TOOLCHAIN_DLLS "${CMAKE_BINARY_DIR}/vcpkg_installed/${VCPKG_TARGET_TRIPLET}/bin/*.dll")
 foreach(TOOLCHAIN_DLL ${TOOLCHAIN_DLLS})
   get_filename_component(TOOLCHAIN_DLL_NAME "${TOOLCHAIN_DLL}" NAME)
   configure_file(${TOOLCHAIN_DLL} "${CMAKE_BINARY_DIR}/bin/${TOOLCHAIN_DLL_NAME}" COPYONLY)
 endforeach()
 
+# Cache
 find_program(CCACHE NAMES ccache PATHS ENV PATH NO_DEFAULT_PATH)
 if(CCACHE)
   set(CMAKE_CXX_COMPILER_LAUNCHER "ccache;cache_dir=${CMAKE_SOURCE_DIR}/.cache/ccache" CACHE STRING "")
   set(CMAKE_C_COMPILER_LAUNCHER "ccache;cache_dir=${CMAKE_SOURCE_DIR}/.cache/ccache" CACHE STRING "")
 endif()
 
+# Directories
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib")
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib")
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin")
